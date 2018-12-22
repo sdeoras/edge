ac7293783cbe4ba4f5ddab4ed44a106bfd637afc
Saurabh Deoras <sdeoras@gmail.com>
2018-12-22 09:37:40 -0800

    adding client services
    Signed-off-by: Saurabh Deoras <sdeoras@gmail.com>

--- consul/server/00/01-sidecar.sh
+++ consul/server/00/01-sidecar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 consul connect proxy -service socat-client-rpi-0 -upstream socat-rpi-0:9190 &
-consul connect proxy -service socat-client-rpi-1 -upstream socat-rpi-0:9191 &
-consul connect proxy -service socat-client-rpi-2 -upstream socat-rpi-0:9192 &
+consul connect proxy -service socat-client-rpi-1 -upstream socat-rpi-1:9191 &
+consul connect proxy -service socat-client-rpi-2 -upstream socat-rpi-2:9192 &
 
 consul connect proxy -service monitor-client-rpi-0 -upstream monitor-rpi-0:60050 &
 consul connect proxy -service monitor-client-rpi-1 -upstream monitor-rpi-1:60051 &
