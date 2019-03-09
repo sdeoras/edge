2a810288707677b4b73e626a523ff5d5592367e5
Saurabh Deoras <sdeoras@gmail.com>
2019-03-08 21:13:56 -0800

    added health check via health api
    Signed-off-by: Saurabh Deoras <sdeoras@gmail.com>

--- go.mod
+++ go.mod
@@ -2,11 +2,11 @@ module github.com/sdeoras/edge
 
 require (
 	cloud.google.com/go v0.36.0
-	github.com/gogo/protobuf v1.2.1
 	github.com/golang/protobuf v1.3.0
 	github.com/hashicorp/golang-lru v0.5.1 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/sdeoras/api v0.1.1
+	github.com/sdeoras/health v0.1.4
 	github.com/sdeoras/jwt v0.0.4
 	github.com/sirupsen/logrus v1.3.0
 	github.com/stretchr/testify v1.3.0 // indirect
@@ -15,7 +15,5 @@ require (
 	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
 	golang.org/x/net v0.0.0-20190301231341-16b79f2e4e95
 	golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421 // indirect
-	golang.org/x/sys v0.0.0-20190308023053-584f3b12f43e // indirect
-	google.golang.org/genproto v0.0.0-20190307195333-5fe7a883aa19 // indirect
 	google.golang.org/grpc v1.19.0
 )
--- monitor/client/health.go
+++ monitor/client/health.go
@@ -1,36 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"github.com/sdeoras/health"
+	"github.com/sdeoras/jwt"
 	"net/http"
 	"os"
 	"path/filepath"
-
-	"github.com/gogo/protobuf/proto"
-	"github.com/sdeoras/jwt"
-	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func checkHealth(services ...string) (map[string]string, error) {
-	jwtRequestor := jwt.NewManager(os.Getenv("JWT_SECRET_KEY"))
+	jwtManager := jwt.NewManager(os.Getenv("JWT_SECRET_KEY"))
+	healthProvider := health.NewProvider(health.OutputProto, jwtManager, nil)
+
 	out := make(map[string]string)
 
 	for _, service := range services {
-		request := new(healthpb.HealthCheckRequest)
-		request.Service = service
-
-		b, err := proto.Marshal(request)
-		if err != nil {
-			return nil, err
-		}
-
-		req, err := jwtRequestor.Request(http.MethodPost,
-			"https://"+filepath.Join(
-				os.Getenv("GOOGLE_GCF_DOMAIN"),
-				ProjectName,
-				"health",
-			), nil, b)
+		req, err := healthProvider.Request(service, "https://"+filepath.Join(
+			os.Getenv("GOOGLE_GCF_DOMAIN"),
+			ProjectName,
+			health.StdRoute))
 
 		client := &http.Client{}
 		resp, err := client.Do(req)
@@ -38,24 +26,12 @@ func checkHealth(services ...string) (map[string]string, error) {
 			return nil, err
 		}
 
-		b, err = ioutil.ReadAll(resp.Body)
+		_, mesg, err := healthProvider.Response(resp)
 		if err != nil {
 			return nil, err
 		}
 
-		if resp.StatusCode != http.StatusOK {
-			return nil, fmt.Errorf("%s:%s. Mesg:%s",
-				"expected status 200 OK, got", resp.Status, string(b))
-		}
-
-		resp.Body.Close()
-
-		response := new(healthpb.HealthCheckResponse)
-		if err := proto.Unmarshal(b, response); err != nil {
-			return nil, err
-		}
-
-		out[service] = response.Status.String()
+		out[service] = mesg
 	}
 
 	return out, nil
